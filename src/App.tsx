f692f56a65a7cb7e7d356f3a10e91cd8b2f8fb52
farazzshaikh <farazzshaikh@gmail.com>
2025-06-09 19:30:07 +0400

    Update submodule demo-2022-lamina-bunny

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Bounds, Loader, OrbitControls } from '@react-three/drei'
-import Bunny from './Bunny'
-import Blob from './Blob'
-import Marble from './Marble'
-import { Debug, Physics } from '@react-three/cannon'
-import Floor from './Floor'
-import TextComponent from './Text'
-import Tag from './Tag'
-import Lighting from './Lighting'
+import { Physics } from "@react-three/cannon";
+import { Bounds, Loader, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import Blob from "./Blob";
+import Bunny from "./Bunny";
+import Floor from "./Floor";
+import Lighting from "./Lighting";
+import Marble from "./Marble";
+import Tag from "./Tag";
+import TextComponent from "./Text";
 
 function App() {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
   return (
     <>
       <Canvas
@@ -25,10 +25,10 @@ function App() {
         }}
         style={{
           opacity: loaded ? 1 : 0,
-          transition: 'opacity 200ms ease-in-out',
+          transition: "opacity 200ms ease-in-out",
         }}
       >
-        <color args={['#2114db']} attach="background" />
+        <color args={["#2114db"]} attach="background" />
 
         <Suspense fallback={null}>
           <Physics>
@@ -49,8 +49,14 @@ function App() {
         </Suspense>
         <Lighting />
 
-        <gridHelper args={[200, 100, '#1100ff', '#1100ff']} position={[0, -1.26, 0]} />
-        <gridHelper args={[200, 1000, '#1100ff', '#1100ff']} position={[0, -1.27, 0]} />
+        <gridHelper
+          args={[200, 100, "#1100ff", "#1100ff"]}
+          position={[0, -1.26, 0]}
+        />
+        <gridHelper
+          args={[200, 1000, "#1100ff", "#1100ff"]}
+          position={[0, -1.27, 0]}
+        />
         {/* <OrbitControls enablePan={false} minZoom={90} maxPolarAngle={Math.PI / 2 - 0.1} minPolarAngle={0} /> */}
         <OrbitControls />
       </Canvas>
@@ -58,7 +64,7 @@ function App() {
 
       <Loader />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
